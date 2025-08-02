bf9797d7eb8b96832f4ecb3390e5c0891ced40f3
Amine TABOU <amin.tabou@gmail.com>
2025-08-02 16:35:46 +0200

    Update version to 1.2.6 and refactor ProductionUnit component to accept props

--- src/components/ProductionUnit.tsx
+++ src/components/ProductionUnit.tsx
@@ -1,14 +1,29 @@
 import { Input } from "./Input";
 import { Switch } from "./Switch";
-
-export const ProductionUnit = () => {
+interface ProductionUnitProps {
+  onChangeInput?: (value: string) => void;
+  onChangeSwitch?: (checked: boolean) => void;
+  value?: string;
+  checked?: boolean;
+}
+export const ProductionUnit = ({
+  onChangeInput,
+  onChangeSwitch,
+  value,
+  checked,
+}: ProductionUnitProps) => {
   return (
     <div className="production-unit-container">
-      <Input label="Production Unit" type="number" />
+      <Input
+        label="Production Unit"
+        type="number"
+        onChange={onChangeInput}
+        value={value}
+      />
       <Switch
         label="Production Unit"
-        checked={true}
-        onChange={(checked) => console.log("Switch toggled:", checked)}
+        checked={checked}
+        onChange={onChangeSwitch}
         className="production-unit-switch"
       />
     </div>
